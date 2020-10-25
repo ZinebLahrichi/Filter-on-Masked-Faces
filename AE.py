4b189829aa13ef91cdbfe172f5c68520994f53ae
Zineb Lahrichi <50481124+ZinebLahrichi@users.noreply.github.com>
2020-10-25 12:16:40 +0100

    Add files via upload

---
+++ AE.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 11:53:18 2020
+
+@author: Zineb Lahrichi
+"""
+import torch 
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+
+#Define the Convolutional Autoencoder
+class ConvAutoencoder(nn.Module):
+    def __init__(self):
+        super(ConvAutoencoder, self).__init__()
+       
+        #Encoder
+        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 4, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        #Decoder
+        self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
+        self.t_conv2 = nn.ConvTranspose2d(16, 1, 2, stride=2)
+
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = F.relu(self.t_conv1(x))
+        x = F.sigmoid(self.t_conv2(x))
+              
+        return x
+
+
+#Instantiate the model
+model = ConvAutoencoder()
